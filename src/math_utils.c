4a3c5f35b3b3e4d732f3ce6189f2d8cb18c87d9e
shishir23811 <shishir23811@gmail.com>
2026-03-11 14:27:25 +0530

    Add basic arithmetic functions with a bug fix
    Implemented basic arithmetic functions: add, subtract, and multiply. Fixed bug in subtract function to perform subtraction instead of addition.

---
+++ src/math_utils.c
@@ -0,0 +1,19 @@
+#include "math_utils.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a + b;   // BUG: should be a - b
+}
+
+int multiply(int a, int b) {
+    int result = 0;
+
+    for(int i = 0; i < b; i++) {
+        result += a;
+    }
+
+    return result;
+}
